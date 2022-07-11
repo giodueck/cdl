ebe969e3c6b0083d0397f8a444b1ef05b0a13119
giodueck <giodueck@gmail.com>
2022-07-11 19:40:44 -0400

    Fix memory leak

--- src/C/main.c
+++ src/C/main.c
@@ -631,6 +631,7 @@ int main(int argc, char **argv)
                 // Backwards
                 loss = dl_mse_loss(output, expected);
                 dl_backpropagation(head, loss, alpha);
+                matrix_free(loss);
 
                 // dbg: testing correctness
                 // verification
